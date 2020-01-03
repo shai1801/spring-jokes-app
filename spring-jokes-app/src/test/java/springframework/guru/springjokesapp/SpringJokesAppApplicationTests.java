4020fd148ef2c64bbe8007d50d3d2e46ac380e4a
Shailendra <shai1801@gmail.com>
2020-01-04 00:12:37 +0530

    Created the intializer project. Closes #1.

---
+++ spring-jokes-app/src/test/java/springframework/guru/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.guru.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
